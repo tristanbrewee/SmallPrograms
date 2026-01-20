9cdbd74a67f1d6b2ab8fad1ab8669d51960db7b0
tristan <tristanbrewee@gmail.com>
2026-01-20 21:08:21 +0100

    BMI calculator meddages added

--- bmiCalculator.py
+++ bmiCalculator.py
@@ -4,3 +4,11 @@ weight = float(input("What is your weight?"))
 bmi = weight / (height ** 2)
 
 print(round(bmi, 2))
+
+if bmi < 18.5:
+    print("You are underweight")
+elif bmi < 25:
+    print("You are normal")
+else:
+    print("You are overweight")
+
